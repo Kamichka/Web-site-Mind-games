826756a0ccfa5eaeeaf63a35e27cad30fd45cabf
dianarybak <164467673+dianarybak@users.noreply.github.com>
2024-04-26 23:50:11 +0300

    Added controllers

---
+++ Controllers/CrosswordController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.DataContext;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CrosswordController : Controller
+    {
+
+       // Общий метод для получения данных кроссворда из базы данных, избегаем дублирования кода
+
+        // Действие для отображения главной страницы кроссвордов
+        public ActionResult Index()
+        {
+             return View("Index");
+        }
+
+
+    }
+}
---
+++ Controllers/QuizController.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using WebApplication2.DataContext;
+
+namespace WebApplication2.Controllers
+{
+    public class QuizController : Controller
+    {
+        // Действие для отображения главной страницы викторин
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+    }
+}
